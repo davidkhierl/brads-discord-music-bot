700d0b7ebfd7b9335a2281ef41e59d7ec134e5ce
davidkhierl <hi@davidkhierl.com>
2022-07-24 06:40:19 +0400

    remove console log

--- src/helpers/dirResolver.ts
+++ src/helpers/dirResolver.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from 'url';
 
 function dirResolver(path: string, ...paths: string[]): string {
 	const fileName = fileURLToPath(path);
-	console.log(fileName);
 	return join(dirname(fileName), ...paths);
 }
 
