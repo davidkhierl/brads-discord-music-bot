f85a5cc8e735b773081bc95d96a80347eac188ae
davidkhierl <hi@davidkhierl.com>
2022-07-26 23:45:54 +0400

    update commands

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -4,36 +4,3 @@ import Frenny from './core/Frenny.js';
 
 Frenny.setBotsCollection([new FrennyAutomateBot(), new FrennyDJBot()]);
 Frenny.deployCommands();
-
-// Frenny.setBotsCollection([new FrennyAutomateBot()]);
-// import { REST } from '@discordjs/rest';
-// import { Routes } from 'discord-api-types/v10';
-// const rest = new REST({ version: '10' }).setToken(
-// 	process.env.DISCORD_FRENNY_AUTOMATE_BOT_TOKEN
-// );
-// rest.put(
-// 	Routes.applicationGuildCommands(
-// 		process.env.DISCORD_FRENNY_AUTOMATE_APP_ID,
-// 		process.env.DISCORD_FRENNY_DEV_GUILD_ID
-// 	),
-// 	{ body: [] }
-// )
-// 	.then(() => console.log('Successfully deleted all guild commands.'))
-// 	.catch(console.error);
-
-// import { REST } from '@discordjs/rest';
-// import { Routes } from 'discord-api-types/v10';
-
-// const rest = new REST({ version: '10' }).setToken(
-// 	process.env.DISCORD_FRENNY_DJ_BOT_TOKEN
-// );
-
-// rest.put(
-// 	Routes.applicationGuildCommands(
-// 		process.env.DISCORD_FRENNY_DJ_APP_ID,
-// 		process.env.DISCORD_FRENNY_DEV_GUILD_ID
-// 	),
-// 	{ body: [] }
-// )
-// 	.then(() => console.log('Successfully deleted all guild commands.'))
-// 	.catch(console.error);
