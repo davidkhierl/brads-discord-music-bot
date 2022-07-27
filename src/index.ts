33c30e85315219bd93c6ad0d7f0693daec01b49f
davidkhierl <hi@davidkhierl.com>
2022-07-28 03:20:12 +0400

    fix: sentry conditional env

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import '@sentry/tracing';
 const releaseName = await getSentryReleaseName();
 
 Sentry.init({
-	environment: process.env.NODE_ENV,
+	environment: process.env.NODE_ENV ?? 'development',
 	dsn: process.env.SENTRY_DSN,
 	release: releaseName,
 	tracesSampleRate: 1.0,
