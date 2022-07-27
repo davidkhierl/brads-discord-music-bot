bbef3d1deda52b6efbcf84d6fe4e2b4003972f68
davidkhierl <hi@davidkhierl.com>
2022-07-28 03:11:36 +0400

    feat: improve sentry dynamic release name

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import FrennyAutomateBot from './bots/FrennyAutomateBot/FrennyAutomateBot.js';
 import FrennyDJBot from './bots/FrennyDJBot/FrennyDJBot.js';
 import Frenny from './core/Frenny.js';
 import { __rootdir__ } from './root.js';
+import getSentryReleaseName from './utils/getSentryReleaseName.js';
 import { RewriteFrames } from '@sentry/integrations';
 import * as Sentry from '@sentry/node';
 import '@sentry/tracing';
@@ -9,10 +10,12 @@ import '@sentry/tracing';
 /**
  * Initialize Sentry
  */
+const releaseName = await getSentryReleaseName();
+
 Sentry.init({
 	environment: process.env.NODE_ENV,
 	dsn: process.env.SENTRY_DSN,
-	release: process.env.npm_package_version,
+	release: releaseName,
 	tracesSampleRate: 1.0,
 	integrations: [
 		new RewriteFrames({
