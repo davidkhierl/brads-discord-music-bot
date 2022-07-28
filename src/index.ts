650b79913e36e3b2c41b0b0b43f8aa2689e9262b
davidkhierl <hi@davidkhierl.com>
2022-07-29 02:57:17 +0400

    feat: added prisma integration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import FrennyAutomateBot from './bots/FrennyAutomateBot/FrennyAutomateBot.js';
 import FrennyDJBot from './bots/FrennyDJBot/FrennyDJBot.js';
 import Frenny from './core/Frenny.js';
+import prisma from './lib/prisma.js';
 import { __rootdir__ } from './root.js';
 import getSentryReleaseName from './utils/getSentryReleaseName.js';
 import { RewriteFrames } from '@sentry/integrations';
 import * as Sentry from '@sentry/node';
-import '@sentry/tracing';
+import * as Tracing from '@sentry/tracing';
 
 /**
  * Initialize Sentry
@@ -26,6 +27,7 @@ Sentry.init({
 			root: process.cwd(),
 			prefix: 'src/',
 		}),
+		new Tracing.Integrations.Prisma({ client: prisma }),
 	],
 });
 
