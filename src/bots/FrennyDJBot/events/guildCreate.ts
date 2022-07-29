a3b724dc854373da4eb64254d92a40b11da6004b
davidkhierl <dkrgidor@gmail.com>
2022-07-29 23:11:06 +0400

    fix: remove saving of roles and added sentry

--- src/bots/FrennyDJBot/events/guildCreate.ts
+++ src/bots/FrennyDJBot/events/guildCreate.ts
@@ -1,6 +1,8 @@
 import { BotEvent, BotEventError } from '../../../core/BotWithCommands.js';
+import SentryHelper from '../../../helpers/SentryHelper.js';
 import saveGuild from '../../../services/saveGuild.js';
 import saveGuildRoles from '../../../services/saveGuildRoles.js';
+import * as Sentry from '@sentry/node';
 import { Guild } from 'discord.js';
 
 /**
@@ -10,18 +12,33 @@ import { Guild } from 'discord.js';
 const guildCreate: BotEvent<Guild> = {
 	name: 'guildCreate',
 	execute: async (guild) => {
-		if (!guild.available) throw new BotEventError('Guild not available');
+		const transaction = SentryHelper.startBotEventTransaction({
+			bot: guild.client.user?.username,
+			op: 'guildCreate',
+		});
 
-		// save the guild to database
-		await saveGuild(guild);
+		Sentry.setContext('guild', {
+			id: guild.id,
+			name: guild.name,
+		});
 
-		// filter only user created roles
-		const roles = guild.roles.cache
-			.filter((role) => !role.managed && role.name !== '@everyone')
-			.toJSON();
+		try {
+			if (!guild.available)
+				throw new BotEventError('Guild not available');
 
-		// save roles to database and connect it to guild
-		await saveGuildRoles(roles);
+			// save the guild to database
+			await saveGuild(guild);
+
+			transaction.setStatus('ok');
+		} catch (error) {
+			if (error instanceof Error) console.log(error.message);
+
+			transaction.setStatus('internal_error');
+
+			Sentry.captureException(error);
+		}
+
+		transaction.finish();
 	},
 };
 
