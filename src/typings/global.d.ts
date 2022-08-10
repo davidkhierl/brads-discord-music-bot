6b56c8bd04099f6da4398bd51ca7e486edf9e455
davidkhierl <dkrgidor@gmail.com>
2022-08-11 01:11:58 +0400

    fix: global typings

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -7,11 +7,15 @@ declare global {
 		interface ProcessEnv {
 			NODE_ENV: string;
 			DEPLOY_COMMANDS?: 'guild' | 'global';
+			DATABASE_URL: string;
 			DISCORD_APP_ID: string;
 			DISCORD_PUB_KEY: string;
 			DISCORD_BOT_TOKEN: string;
 			DISCORD_DEVELOPMENT_GUILD_ID: string;
+			SENTRY_AUTH_TOKEN: string;
 			SENTRY_DSN: string;
+			SENTRY_ORG: string;
+			SENTRY_PROJECT: string;
 			SERVER_NAME?: string;
 		}
 
