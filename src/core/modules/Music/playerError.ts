70a6b1e758eb1730aa430b31aef13ecdcb59b36f
davidkhierl <dkrgidor@gmail.com>
2022-08-20 01:42:05 +0400

    fix: remove queue stop

--- src/core/modules/Music/playerError.ts
+++ src/core/modules/Music/playerError.ts
@@ -2,6 +2,4 @@ import { MusicQueue } from './Music.js';
 
 export const playerError = (error: string, queue: MusicQueue) => {
 	console.log('Player Error: ', error);
-
-	if (queue) queue.stop();
 };
